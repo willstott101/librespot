369b24249b1845eafe41184a88323af996c9fd29
Will Stott <willstott101@gmail.com>
2019-03-14 19:49:47 +0000

    Protobuf 2.4.0, generate all proto files every time, but only write when changed.

--- protocol/build.rs
+++ protocol/build.rs
@@ -1,62 +1,54 @@
-extern crate protobuf_codegen_pure;
+extern crate protobuf_codegen; // Does the business
+extern crate protobuf_codegen_pure; // Helper function
 extern crate regex;
-extern crate crc;
-use protobuf_codegen_pure::Customize;
-use regex::Regex;
-use std::path::Path;
-use std::fs::File;
-use std::io::prelude::*;
-use crc::crc32::checksum_ieee;
 
-fn read_to_string(path: String) -> Option<std::string::String> {
-    let mut file = File::open(path).unwrap();
-    let mut f_str = String::new();
-    file.read_to_string(&mut f_str).unwrap();
-    Some(f_str)
-}
+use std::path::Path;
+use std::fs::{read_to_string, write};
+
+use protobuf_codegen_pure::Customize;
+use protobuf_codegen_pure::parse_and_typecheck;
+
+use regex::Regex;
 
 fn main() {
-    let line_re = Regex::new(r"pub mod (?P<name>\w+);(?: ?/?/? ?(?P<CRC>\d+)?)?(?P<crlf>\r?\n)").unwrap();
+    let customizations = Customize { ..Default::default() };
 
-    let mut lib_str = read_to_string("src/lib.rs".to_string()).unwrap();
-    let mut changed = false;
+    let line_re = Regex::new(r"pub mod (\w+);").unwrap();
+    let lib_str = read_to_string("src/lib.rs").unwrap();
 
-    for cap in line_re.captures_iter(&lib_str.clone()) {
-        let name = cap.name("name").unwrap().as_str();
+    // Iterate over the desired module names.
+    for cap in line_re.captures_iter(&lib_str) {
+        let name = cap.get(1).unwrap().as_str();
 
+        // Build the paths to relevant files.
+        let src = &format!("proto/{}.proto", name);
         let dest = &format!("src/{}.rs", name);
-        let mut regen = !Path::new(dest).exists();
 
-        let path = &format!("proto/{}.proto", name);
-        let contents = read_to_string(path.to_string()).unwrap().replace("\r\n", "\n");
-        let new_crc = checksum_ieee(&contents.into_bytes()).to_string();
-        if !regen {
-            match cap.name("CRC") {
-                Some(crc) => regen = crc.as_str() != new_crc,
-                None      => regen = true,
-            }
+        // Get the contents of the existing generated file.
+        let mut existing = "".to_string();
+        if Path::new(dest).exists() {
+            // Removing CRLF line endings if present.
+            existing = read_to_string(dest).unwrap().replace("\r\n", "\n");
         }
 
-        if regen {
-            println!("Regenerating {} from {}", dest, path);
-            protobuf_codegen_pure::run(protobuf_codegen_pure::Args {
-                out_dir: "src",
-                input: &[path],
-                includes: &["proto"],
-                customize: Customize { ..Default::default() },
-            }).expect("protoc");
+        println!("Regenerating {} from {}", dest, src);
 
-            changed = true;
-            let line_ending = cap.name("crlf").unwrap().as_str();
-            lib_str = lib_str.replace(&cap[0], &format!("pub mod {}; // {}{}", name, new_crc, line_ending));
-        } else {
-            println!("CRC Matches for {}.proto not re-building.", name);
+        // Parse the proto files as the protobuf-codegen-pure crate does.
+        let p = parse_and_typecheck(&["proto"], &[src]).expect("protoc");
+        // But generate them with the protobuf-codegen crate directly.
+        // Then we can keep the result in-memory.
+        let result = protobuf_codegen::gen(
+            &p.file_descriptors,
+            &p.relative_paths,
+            &customizations,
+        );
+        // Protoc result as a byte array.
+        let new = &result.first().unwrap().content;
+        // Convert to utf8 to compare with existing.
+        let new = std::str::from_utf8(&new).unwrap();
+        // Save newly generated file if changed.
+        if new != existing {
+            write(dest, &new).unwrap();
         }
-    }
-
-    if changed {
-        // Write new checksums to file
-        let mut file = File::create("src/lib.rs").unwrap();
-        file.write_all(lib_str.as_bytes()).unwrap();
     }
 }
--- protocol/src/lib.rs
+++ protocol/src/lib.rs
@@ -1,10 +1,9 @@
 extern crate protobuf;
 // This file is parsed by build.rs
 // Each included module will be compiled from the matching .proto definition.
-// A CRC of the used proto file will be added as a comment.
-pub mod authentication; // 284352592
-pub mod keyexchange; // 1910242467
-pub mod mercury; // 3661728422
-pub mod metadata; // 3464762495
-pub mod pubsub; // 2935982216
-pub mod spirc; // 2909797125
+pub mod authentication;
+pub mod keyexchange;
+pub mod mercury;
+pub mod metadata;
+pub mod pubsub;
+pub mod spirc;
